1876584de41b480fe4a6feac0d7acd9e419d0afd
AKepska <ola@kepska.pl>
2023-04-12 19:35:51 +0200

    add booleans, checking methods, refactor loops

--- src/main/java/com/github/arena/challenges/weakmdparser/MarkdownParser.java
+++ src/main/java/com/github/arena/challenges/weakmdparser/MarkdownParser.java
@@ -1,80 +1,98 @@
 package com.github.arena.challenges.weakmdparser;
 
 public class MarkdownParser {
+	boolean isList;
+	boolean isHeader;
+	boolean isParagraph;
 
-    String parse(String markdown) {
-        String[] lines = markdown.split("\n");
-        String result = "";
-        boolean activeList = false;
+	String parse(String markdown) {
+		String[] splitText = markdown.split("\n");
+		String result = "";
+		boolean activeList = false;
 
-        for (int i = 0; i < lines.length; i++) {
 
-            String theLine = ph(lines[i]);
+		for (int i = 0; i < splitText.length; i++) {
+			String theLineForCheckUp = splitText[i];
+			String theLineAfterHeaderCheckUp = checkIfHeader(theLineForCheckUp);
+			String theLineAfterListCheckUp = (!isHeader) ? checkIfList(theLineForCheckUp) : checkIfList(theLineAfterHeaderCheckUp);
+			String theLineAfterParagraphCheckUp = (isList && isHeader)
+					? theLineAfterListCheckUp : ((isHeader && !isList) ? theLineAfterHeaderCheckUp
+					: ((!isHeader && isList) ? theLineAfterListCheckUp : checkIfParagraph(theLineForCheckUp)));
 
-            if (theLine == null) {
-                theLine = li(lines[i]);
-            }
+			if (isList && !isHeader && !isParagraph && !activeList) {
+				activeList = true;
+				result = result + "<ul>";
+				result = result + theLineAfterParagraphCheckUp;
+			} else if (!isList && activeList) {
+				activeList = false;
+				result = result + "</ul>";
+				result = result + theLineAfterParagraphCheckUp;
+			} else {
+				result = result + theLineAfterParagraphCheckUp;
+			}
+		}
+		if (activeList) {
+			result = result + "</ul>";
+		}
+		return result;
+	}
 
-            if (theLine == null) {
-                theLine = p(lines[i]);
-            }
+	private String checkIfHeader(String splitTextPart) {
+		String theLine = ph(splitTextPart);
+		isHeader = true;
 
-            if (theLine.matches("(<li>).*") && !theLine.matches("(<h).*") && !theLine.matches("(<p>).*") && !activeList) {
-                activeList = true;
-                result = result + "<ul>";
-                result = result + theLine;
-            } else if (!theLine.matches("(<li>).*") && activeList) {
-                activeList = false;
-                result = result + "</ul>";
-                result = result + theLine;
-            } else {
-                result = result + theLine;
-            }
-        }
+		if (theLine == null) {
+			isHeader = false;
+		}
+		return theLine;
+	}
 
-        if (activeList) {
-            result = result + "</ul>";
-        }
+	private String checkIfList(String splitTextPart) {
+		isList = true;
+		String theLine = li(splitTextPart);
+		if (theLine == null) {
+			isList = false;
+		}
+		return theLine;
+	}
 
-        return result;
-    }
+	private String checkIfParagraph(String splitTextPart) {
+		isParagraph = true;
+		return "<p>" + parseSomeSymbols(splitTextPart) + "</p>";
+	}
 
-    protected String ph(String markdown) {
-        int count = 0;
+	protected String ph(String markdown) {
+		int count = 0;
 
-        for (int i = 0; i < markdown.length() && markdown.charAt(i) == '#'; i++) {
-            count++;
-        }
+		for (int i = 0; i < markdown.length() && markdown.charAt(i) == '#'; i++) {
+			count++;
+		}
 
-        if (count == 0) {
-            return null;
-        }
+		if (count == 0) {
+			return null;
+		}
 
-        return "<h" + Integer.toString(count) + ">" + markdown.substring(count + 1) + "</h" + Integer.toString(count) + ">";
-    }
+		return "<h" + count + ">" + markdown.substring(count + 1) + "</h" + count + ">";
+	}
 
-    public String li(String markdown) {
-        if (markdown.startsWith("*")) {
-            String skipAsterisk = markdown.substring(2);
-            String listItemString = parseSomeSymbols(skipAsterisk);
-            return "<li>" + listItemString + "</li>";
-        }
+	public String li(String markdown) {
+		if (markdown.startsWith("*")) {
+			String skipAsterisk = markdown.substring(2);
+			String listItemString = parseSomeSymbols(skipAsterisk);
+			return "<li>" + listItemString + "</li>";
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    public String p(String markdown) {
-        return "<p>" + parseSomeSymbols(markdown) + "</p>";
-    }
+	public String parseSomeSymbols(String markdown) {
 
-    public String parseSomeSymbols(String markdown) {
+		String lookingFor = "__(.+)__";
+		String update = "<strong>$1</strong>";
+		String workingOn = markdown.replaceAll(lookingFor, update);
 
-        String lookingFor = "__(.+)__";
-        String update = "<strong>$1</strong>";
-        String workingOn = markdown.replaceAll(lookingFor, update);
-
-        lookingFor = "_(.+)_";
-        update = "<em>$1</em>";
-        return workingOn.replaceAll(lookingFor, update);
-    }
+		lookingFor = "_(.+)_";
+		update = "<em>$1</em>";
+		return workingOn.replaceAll(lookingFor, update);
+	}
 }
